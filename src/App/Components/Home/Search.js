5cbfca5453138df638896647ddbab6d0eefe37c4
Onejohi <tonnie254@live.com>
2022-06-20 20:57:45 +0300

    - attempt to use unsafe url

--- src/App/Components/Home/Search.js
+++ src/App/Components/Home/Search.js
@@ -12,7 +12,7 @@ function Search() {
     async function fetchSchools () {
       setLoading(true);
       const data = await fetch(`http://universities.hipolabs.com/search?name=${debouncedSearch}`, {
-        mode: 'no-cors',
+        referrerPolicy: 'unsafe-url',
       })
         .then((res) => res.json());
       setSchools(data);
